d47e078b63b06a359c4bc2d5a0f7671dfa68dc3d
Dalton Harmsen <daltonrharmsen@gmail.com>
2025-06-14 15:33:20 +0200

    updated _model_call extension from HFLM for compatibility with OLMo

--- lm_eval/models/hf_olmo.py
+++ lm_eval/models/hf_olmo.py
@@ -128,3 +128,29 @@ class OLMoLM(HFLM):
 
         if add_bos_token:
             self.tokenizer.add_bos_token = True
+
+    def _model_call(self, inps, attn_mask=None, labels=None):
+        """
+        :param inps: torch.Tensor
+            A torch tensor of shape [batch, (sequence_ctx + sequence_cont)] or of shape
+            [batch, sequence_ctx]. the size of sequence may vary from call to call
+        :param attn_mask: torch.Tensor, optional
+            A torch tensor of shape [batch, (sequence_ctx + sequence_cont)]. Only passed
+            (and must be passed) if self.AUTO_MODEL_CLASS is transformers.AutoModelForSeq2SeqLM
+        :param labels: torch.Tensor, optional
+            A torch tensor of shape [batch, (sequence_ctx + sequence_cont)]. Only passed
+            (and must be passed) if self.AUTO_MODEL_CLASS is transformers.AutoModelForSeq2SeqLM
+        :return
+            A torch tensor of shape [batch, sequence, vocab] with the
+        logits returned from the model's decoder
+        """
+        with torch.no_grad():
+            if attn_mask is not None or labels is not None:
+                assert attn_mask is not None and labels is not None
+                assert self.AUTO_MODEL_CLASS == OLMoForCausalLM
+                return self.model(
+                    input_ids=inps, attention_mask=attn_mask, labels=labels
+                ).logits
+            else:
+                assert self.AUTO_MODEL_CLASS == OLMoForCausalLM
+                return self.model(inps).logits
